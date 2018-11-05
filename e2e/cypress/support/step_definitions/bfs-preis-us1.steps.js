035277ed00e68fa12434550b3ef24796dc802709
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-11-05 16:12:52 +0100

    Added structure of feature implementations

---
+++ e2e/cypress/support/step_definitions/bfs-preis-us1.steps.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+/// <reference types="cucumber"/>
+
+Given('ein Lohn von {float} CHF', lohn => {
+    throw Error('not yet implemented')
+});
+
+Given('ein Startdatum im {string}', startdatum => {
+    throw Error('not yet implemented');
+});
+
+Given('ein Zieldatum im {string}', zieldatum => {
+    throw Error('not yet implemented');
+});
+
+When('ich die Teuerung für die Index-Basis: {string} berechne', index => {
+    throw Error('not yet implemented');
+});
+
+Then('wird ein Betrag von {float} CHF', betrag => {
+    throw Error('not yet implemented');
+});
+
+Then('eine Veränderung von {float} % angezeigt', veraenderung => {
+    throw Error('not yet implemented');
+});
+
+Then('werden folgende Indexe aufgelistet', werte => {
+    throw Error('not yet implemented');
+});
